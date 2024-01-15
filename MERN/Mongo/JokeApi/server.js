5d23a545d641a888c17bfd5fe33fa84ca9733b34
NestorRiberoPUJ1 <67485144+NestorRiberoPUJ1@users.noreply.github.com>
2024-01-15 07:15:43 -0500

    update

---
+++ MERN/Mongo/JokeApi/server.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+const app = express();
+const port = 8000;
+
+require('./config/mongoose.config');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+const bromaRoutes = require('./routes/broma.routes');
+app.use("/api/jokes", bromaRoutes);
+
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
